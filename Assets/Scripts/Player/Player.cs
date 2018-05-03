c5cf6446cf90b2bd41357b6e7d1df19a6eab17f2
Savran Taha <savrantaha@hotmail.com>
2018-05-03 10:19:44 +0200

    test

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+    private SpriteHolder spriteHolder;
+
+    public Player()
+    {
+        spriteHolder = new SpriteHolder();
+    }
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        move();
+
+	}
+    void move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        int a = 
+    }
+}
---
+++ Assets/Scripts/Player/SpriteHolder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteHolder : MonoBehaviour
+{
+    private int currentSpriteState = 0;
+
+    public Sprite[] Sprites_NotMoving;
+    public Sprite[] Sprites_left;
+    public Sprite[] Sprites_right;
+    public Sprite[] Sprites_top;
+    public Sprite[] Sprites_down;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    public void SetDirection(int code)
+    {
+        currentSpriteState = code;
+    }
+    public void GetCurrentSprite()
+    {
+
+    }
+    private Sprite[] getSpriteArray()
+    {
+        switch (currentSpriteState)
+        {
+            case 0: return Sprites_NotMoving;
+            case 1: return Sprites_left;
+            case 2: return Sprites_right;
+            case 3: return Sprites_top;
+            case 4: return Sprites_down;
+
+            default: return null;
+        }
+
+        return Sprites_left;
+    }
+}
