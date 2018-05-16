ec7de09cf20ad9a8e8035cccf41f9af2ed326d33
Savran Taha <savrantaha@hotmail.com>
2018-05-16 18:51:34 +0200

    Added functions for PipeGame

---
+++ Assets/Scripts/Pipe/Pipe.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+    public int PipeIsLookingTo = 0;
+    public int PipeHasToLookTo = 0;
+    public bool DirectionDoesntMather = false;
+
+    private int clicks = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+
+        clicks++;
+
+        changeLookingDirection();
+    }
+    private void changeLookingDirection()
+    {
+        if (PipeIsLookingTo < 3) PipeIsLookingTo++;
+        else PipeIsLookingTo = 0;
+
+        rotate();
+    }
+    private void rotate()
+    {
+        Quaternion rot = gameObject.transform.rotation;
+
+        int i = 0;
+
+        switch (PipeIsLookingTo)
+        {
+            case 0: i = 0; break;
+            case 1: i = 270; break;
+            case 2: i = 180; break;
+            case 3: i = 90; break;
+        }
+
+        rot.z = i;
+        //gameObject.transform.rotation = rot;
+
+        transform.Rotate(0, 0, -90);
+    }
+    public int GetClicks()
+    {
+        int a = clicks;
+        clicks = 0;
+        return a;
+    }
+    public bool SitsGood()
+    {
+        return DirectionDoesntMather | PipeIsLookingTo == PipeHasToLookTo;
+    }
+}
---
+++ Assets/Scripts/Pipe/PipeGameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGameManager : MonoBehaviour {
+
+    public int MovesOver = 999;
+    private int movesDone = 0;
+
+    public Pipe[] pipeArray;
+    public bool gameFinnished = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        checkCLicks();
+
+        bool a = true;
+        foreach(Pipe pipe in pipeArray)
+        {
+            a &= pipe.SitsGood();
+        }
+
+        gameFinnished |= a;
+
+        checkStatsus();
+	}
+    private void checkCLicks()
+    {
+        int clicks = 0;
+
+        foreach(Pipe pipe in pipeArray)
+        {
+            clicks += pipe.GetClicks();
+        }
+
+        movesDone += clicks;
+        MovesOver -= clicks;
+    }
+    private void checkStatsus()
+    {
+
+    }
+}
