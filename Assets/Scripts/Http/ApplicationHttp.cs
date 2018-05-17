3d07acfab30de2b7e87c3a90f6d3118ad1a8dc08
Savran Taha <savrantaha@hotmail.com>
2018-05-17 11:08:58 +0200

    Added http class

---
+++ Assets/Scripts/Http/ApplicationHttp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApplicationHttp : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+        
+	}
+    private void Post()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("data=value"));
+
+        UnityWebRequest www = UnityWebRequest.Post("http://www.tahayasin.be", formData);
+        UnityWebRequest wwww = UnityWebRequest.Get("http://www.tahayasin.be");
+
+        StartCoroutine(Excecute(www));
+    }
+
+    IEnumerator Excecute(UnityWebRequest www)
+    {
+        if (www == null) Debug.LogWarning("WWW is empty");
+
+        yield return www.SendWebRequest();
+
+        if(www.isHttpError || www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string text = www.downloadHandler.text;
+            Debug.Log(text);
+        }
+    }
+}
